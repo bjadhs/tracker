045b0c7e5622b4c87034a7886174644bf6d14654
Bijaya Adhikari <bjadhs@gmail.com>
2025-02-15 23:19:47 +1100

    Connected to database mongodb atlas

--- tracker/app.js
+++ tracker/app.js
@@ -3,15 +3,18 @@ import {PORT} from './config/env.js';
 import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js';  
 import subscriptionRouter from './routes/subscription.routes.js'; 
+import connectToDB from './database/mongodb.js';
 
 const app = express();
 
-app.use('/api/auth', authRouter);
-app.use('/api/user', userRouter);
-app.use('/api/subscriptions', subscriptionRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
 
-app.listen(PORT, () => { 
+app.listen(PORT, async () => { 
     console.log(`Server is running on http://localhost:${PORT}`);
+    await connectToDB();
+
     });
 
 export default app;
